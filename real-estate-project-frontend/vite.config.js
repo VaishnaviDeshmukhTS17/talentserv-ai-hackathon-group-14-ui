308a01d8c21edc21a826fec04b10d2c58c42a32a
DivyaM310 <mulikdivya1@gmail.com>
2026-05-25 23:37:27 +0530

    github ci

--- real-estate-project-frontend/vite.config.js
+++ real-estate-project-frontend/vite.config.js
@@ -17,4 +17,23 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes('node_modules/react/') ||
+            id.includes('node_modules/react-dom/') ||
+            id.includes('node_modules/react-router-dom/')
+          ) {
+            return 'vendor';
+          }
+          if (id.includes('node_modules/recharts/')) {
+            return 'charts';
+          }
+        },
+      },
+    },
+  },
 })
